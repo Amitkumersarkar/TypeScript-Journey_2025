3e7441d2d14dd33fe23bd43f0ba46aa410e21a15
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-07 18:19:07 +0600

    added data type file

---
+++ Typescript_Series01/DataType/type.js
@@ -0,0 +1,6 @@
+/* there are lot's of dataType in ts
+such as : number,string,boolean,null,
+undefined,void,object,array,tuples,any,never and unknown.
+*/
+var greeting = "Hello, Amit";
+console.log(greeting);
---
+++ Typescript_Series01/DataType/type.ts
@@ -0,0 +1,7 @@
+/* there are lot's of dataType in ts 
+such as : number,string,boolean,null,
+undefined,void,object,array,tuples,any,never and unknown.
+*/
+
+let greeting: string = "Hello, Amit";
+console.log(greeting);
