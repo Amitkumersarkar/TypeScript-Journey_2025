8ac7f8d8b7b23ef871f1b0623b11eafca39d9066
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-09 21:56:55 +0600

    added arraw function

--- Typescript_Series01/Function/func.ts
+++ Typescript_Series01/Function/func.ts
@@ -9,8 +9,13 @@ function getUpper(val: string) {
 function signUpUser(name: string, email: string, password: number, isPaid: boolean) {
 
 }
+let logInUser = (name: string, email: string, isPaid: boolean) => {
+
+}
 
 addTwo(5)
 getUpper('amit')
 signUpUser('amit', 'amit@gmail.com', 1234, true)
+logInUser('amitsarkar', 'amit@gmail.com', true)
+
 export { }
