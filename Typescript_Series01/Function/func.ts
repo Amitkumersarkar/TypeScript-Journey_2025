2399b88e7a8e9c9d39463e7c0c408ded23b724f6
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-09 21:52:44 +0600

    added function in ts

---
+++ Typescript_Series01/Function/func.ts
@@ -0,0 +1,16 @@
+function addTwo(num: number) {
+    return num + 2
+}
+
+function getUpper(val: string) {
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, password: number, isPaid: boolean) {
+
+}
+
+addTwo(5)
+getUpper('amit')
+signUpUser('amit', 'amit@gmail.com', 1234, true)
+export { }
