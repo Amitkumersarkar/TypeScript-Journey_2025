15e0e4ae0935eeea9e510b9e5c9c357bf8d31b17
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-11 20:05:30 +0600

    after compile the objects

---
+++ Typescript_Series01/Objects/objects01.js
@@ -0,0 +1,12 @@
+var User = {
+    name: "amit",
+    email: "amit@gmail.com",
+    isActive: true
+};
+function createUser(_a) {
+    var name = _a.name, isPaid = _a.isPaid;
+}
+createUser({
+    name: "amit",
+    isPaid: true
+});
