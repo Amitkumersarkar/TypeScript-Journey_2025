24746cedbbc7156f23ac93a4b78aaa161ee5eaea
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-14 19:34:54 +0600

    readOnly type in ts

--- Typescript_Series01/TypeAliases/readOnly.ts
+++ Typescript_Series01/TypeAliases/readOnly.ts
@@ -12,6 +12,17 @@ let myUser: User = {
     email: "h@h.com",
     isActive: false
 }
+type cardNumber = {
+    cardNumber: string
+}
+
+type cardDate = {
+    cardDate: string
+}
+
+type CardDetails = cardNumber & cardDate & {
+    cvv: number
+}
 
 myUser.email = "amit@gmail.com"
 // myUser._id ="Amit"
