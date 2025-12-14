041cec385de98a7053f366c66106954e7e09ea99
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-14 19:05:07 +0600

    type aliases in ts

---
+++ Typescript_Series01/TypeAliases/typeAlias.ts
@@ -0,0 +1,15 @@
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
+
+}
+
+// type MyString = string
+
+function createUser(user: User): User {
+    return { name: "", email: "", isActive: true }
+}
+createUser({ name: "", email: "", isActive: true })
+
+export { }
