af89dadf9ef529448075027c5fd0e7084e5e6b1e
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-06 21:24:57 +0600

    array in ts

---
+++ Typescript_Series01/Array/arrayInTs.ts
@@ -0,0 +1,10 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+type Users = {
+    name: string
+    isActive: boolean
+}
+
+superHeros.push("batMan")
+heroPower.push(2)
