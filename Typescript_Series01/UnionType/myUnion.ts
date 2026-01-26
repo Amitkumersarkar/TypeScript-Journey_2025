118742dd274a6a4a515968bfdb24afe4baf7b7cb
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-26 21:53:22 +0600

    added new function

--- Typescript_Series01/UnionType/myUnion.ts
+++ Typescript_Series01/UnionType/myUnion.ts
@@ -2,3 +2,17 @@ let score: number | string = 33
 
 score = 44
 score = "55"
+
+type User = {
+    name: string;
+    id: number
+}
+
+type Admin = {
+    UserName: string;
+    id: number
+}
+
+let Amit: User | Admin = { name: "Amit", id: 283 }
+Amit = { UserName: "softDev", id: 283 }
+
